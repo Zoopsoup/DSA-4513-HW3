119b9fbfa3ddb93fd28fc774ab4183ed5d2bc554
Tyler Sanbar <tylersanbar@gmail.com>
2021-10-17 14:40:35 -0500

    Ready for templates

--- AzureSQL/src/Problem2.java
+++ AzureSQL/src/Problem2.java
@@ -30,6 +30,7 @@ public class Problem2 {
 	// Query templates    
 	final static String QUERY_TEMPLATE_1 = "INSERT INTO Performer " + "VALUES (?, ?, ?, ?);";
     final static String QUERY_TEMPLATE_2 = "SELECT * FROM Performer;";
+    final static String QUERY_TEMPLATE_3 = "SELECT * FROM Performer;";
     // User input prompt//    
     final static String PROMPT = "\nPlease select one of the options below: \n" + "1) Insert new Performer; \n" + "2) Insert new Performer (with did); \n" + "3) Display all perfomers; \n" + "3) Exit!";
     
@@ -51,18 +52,16 @@ public class Problem2 {
         			// Insert a new student option                    
         			// Collect the new student data from the user                    
         			System.out.println("Please enter integer performer ID:");
-                    final int pid = sc.nextInt();
+                    int pid = sc.nextInt();
                     // Read in the user input of student ID                    
                     System.out.println("Please enter performer name:");
                     // Preceding nextInt, nextFloar, etc. do not consume new line characters from the user input.                    
                     // We call nextLine to consume that newline character, so that subsequent nextLine doesn't return nothing.             
                     sc.nextLine();
-                    final String name = sc.nextLine();
+                    String name = sc.nextLine();
                     // Read in user input of performer name     
-                    System.out.println("Please enter integer years of experience:");
-                    final int yearsOfExperience = sc.nextInt();
                     System.out.println("Please enter integer age:");
-                    final int age = sc.nextInt();
+                    int age = sc.nextInt();
                     // Read in user input of performer age                    
           
                     // Read in user input of performer Classification                    
@@ -73,8 +72,7 @@ public class Problem2 {
                     		// Populate the query template with the data collected from the user                            
                     		statement.setInt(1, pid);
                             statement.setString(2, name);
-                            statement.setInt(3, yearsOfExperience);
-                            statement.setInt(4, age);
+                            statement.setInt(3, age);
                             System.out.println("Dispatching the query...");
                             // Actually execute the populated query                            
                             final int rows_inserted = statement.executeUpdate();
@@ -83,22 +81,40 @@ public class Problem2 {
                     }                    
                     break;
                 case "2":                    
-                	System.out.println("Connecting to the database...");
-                    // Get the database connection, create statement and execute itright away, as no user input need be collected                    
-                	try (final Connection connection = DriverManager.getConnection(URL)) {                        
-                		System.out.println("Dispatching the query...");
-                        try (
-                            final Statement statement = connection.createStatement();
-                            final ResultSet resultSet = statement.executeQuery(QUERY_TEMPLATE_2)) {                                
-                        	System.out.println("Contents of the Student table:");
-                            System.out.println("ID | first name | last name | GPA | major | classification ");
-                            // Unpack the tuples returned by the database and print them out to the user                                
-                            while (resultSet.next()) {                                    
-                            System.out.println(String.format("%s | %s | %s | %s | %s | %s ",resultSet.getString(1),resultSet.getString(2),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5),resultSet.getString(6)));
-                            }
-                        }                        
-                    }                                        
-        		break;
+                	// Insert a new student option                    
+        			// Collect the new student data from the user                    
+        			System.out.println("Please enter integer performer ID:");
+                    pid = sc.nextInt();
+                    // Read in the user input of student ID                    
+                    System.out.println("Please enter performer name:");
+                    // Preceding nextInt, nextFloar, etc. do not consume new line characters from the user input.                    
+                    // We call nextLine to consume that newline character, so that subsequent nextLine doesn't return nothing.             
+                    sc.nextLine();
+                    name = sc.nextLine();
+                    // Read in user input of performer name     
+                    System.out.println("Please enter integer director ID:");
+                    int did = sc.nextInt();
+                    System.out.println("Please enter integer age:");
+                    age = sc.nextInt();
+                    // Read in user input of performer age                    
+          
+                    // Read in user input of performer Classification                    
+                    System.out.println("Connecting to the database...");
+                    // Get a database connection and prepare a query statement                    
+                    try (final Connection connection = DriverManager.getConnection(URL)) {                        
+                    	try (final PreparedStatement statement = connection.prepareStatement(QUERY_TEMPLATE_2)) {                            
+                    		// Populate the query template with the data collected from the user                            
+                    		statement.setInt(1, pid);
+                            statement.setString(2, name);
+                            statement.setInt(3, did);
+                            statement.setInt(4, age);
+                            System.out.println("Dispatching the query...");
+                            // Actually execute the populated query                            
+                            final int rows_inserted = statement.executeUpdate();
+                            System.out.println(String.format("Done. %d rows inserted.", rows_inserted));
+                        }                    
+                    }                    
+                    break;
                 case "3":                    
                 	System.out.println("Connecting to the database...");
                     // Get the database connection, create statement and execute itright away, as no user input need be collected                    
@@ -106,8 +122,8 @@ public class Problem2 {
                 		System.out.println("Dispatching the query...");
                         try (
                             final Statement statement = connection.createStatement();
-                            final ResultSet resultSet = statement.executeQuery(QUERY_TEMPLATE_2)) {                                
-                        	System.out.println("Contents of the Student table:");
+                            final ResultSet resultSet = statement.executeQuery(QUERY_TEMPLATE_3)) {                                
+                        	System.out.println("Contents of the Performer table:");
                             System.out.println("ID | name | years of experience | age ");
                             // Unpack the tuples returned by the database and print them out to the user                                
                             while (resultSet.next()) {                                    

08f144785f0f05777f529fe3408f31a847c06583
Kiley Peters <kiley.m.peters-1@ou.edu>
2021-10-17 11:19:49 -0500

    added problem 1 files (2)

---
+++ HW3_Problem1_Group15.sql
@@ -0,0 +1,15 @@
+---1.1---
+---a)
+---INSERT INTO Movie VALUES 
+---('Jurassic Park','Action',125,1984,2,3.5);
+---b)
+---INSERT INTO Movie VALUES
+---(NULL,'Action',125,1984,2,3.5);
+---c)
+---INSERT INTO Movie VALUES
+---('The Avengers','Action',125,1984,5,3.5);
+---DELETE FROM Director WHERE did = 1;
+
+---UPDATE Movie SET did = 5;
+---d)
+---SELECT dname FROM Director WHERE dname = 5;
